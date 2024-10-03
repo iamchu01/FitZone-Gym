777deb3a782bbdafb5261b787a4d8aca26ed8e3f
Ronel Vincent Nuevo Chu <ronelvincentchu88@gmail.com>
2024-10-03 12:29:45 +0800

    sidebar changes

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -8,29 +8,37 @@ $page = $components[2];
 <div class="sidebar" id="sidebar">
     <div class="sidebar-inner slimscroll">
         <div id="sidebar-menu" class="sidebar-menu">
+
+        
             <ul class="sidebar-vertical">
                 <li class="menu-title"> 
                     <span>Main</span>
                 </li>
 
                 <!--//* DASHBOARD LINK -->
-                <li class="<?php echo ($page == 'admin-dashboard.php')?'active':'';?>" href="admin-dashboard.php"> 
-                    <a href="admin-dashboard.php"><i class="la la-dashboard"></i> <span>Dashboard</span></a>
+               <li class="<?php echo ($page == 'admin-dashboard.php') ?: ''; ?>"> 
+                    <a href="admin-dashboard.php"><i class="la la-home"></i> <span>Dashboard</span></a>
                 </li>
-               
-                <!--//* DASHBOARD LINK -->
+                
+                <hr style="border: 0; border-top: 1px solid #A0A0A0; margin: 10px 0;">
 
                 <!-- //* CALENDAR -->
                  <li class="<?php echo ($page == 'events.php')?'active':'';?>" href="events.php"> 
                     <a href="events.php"><i class="la la-calendar"></i> <span>Calendar</span></a>
                 </li>
-                 <!-- //* CALENDAR -->
+                
+                  <hr style="border: 0; border-top: 1px solid #A0A0A0; margin: 10px 0;">
+
+                 
                   <!-- //* file maintinance -->
                 <li class=" submenu"> 
                 <a href="#" ><i class="la la-box"></i> <span> File maintinance</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a class="<?php echo ($page == 'category-list.php')?'active':'';?>" href="category-list.php">Category List</a></li>                 
+                        <li><a class="<?php echo ($page == 'add-instructor.php')?'active':'';?>" href="add-instructor.php">Add Instructor</a></li>
+                        <li><a class="<?php echo ($page == 'employees-list.php')?'active':'';?>" href="employees-list.php">Add Member</a></li>
+                        <li><a class="<?php echo ($page == 'create-membership.php')?'active':'';?>" href="create-membership.php">Create Membership</a></li>
                         <li><a class="<?php echo ($page == 'inventory.php')?'active':'';?>" href="inventory.php">Product List</a></li>
+                        <li><a class="<?php echo ($page == 'category-list.php')?'active':'';?>" href="category-list.php">Category List</a></li>                 
                         <li><a class="<?php echo ($page == 'offered-programs.php')?'active':'';?>" href="offered-programs.php">Offered Programs</a></li>  
                         <li><a class="<?php echo ($page == 'targeted-exercise.php')?'active':'';?>" href="targeted-exercise.php">Targeted Exercise</a></li>
                     </ul>
@@ -40,40 +48,42 @@ $page = $components[2];
                 <!-- //* INVENTORY -->
 
                 <!-- //* CUSTOMER MANAGEMENT -->
-                <li class="submenu">
+                <!-- <li class="submenu">
                     <a href="#" ><i class="la la-users"></i> <span>Customer Management</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a class="<?php echo ($page == 'employees-list.php')?'active':'';?>" href="employees-list.php">Member List</a></li>
+                        <li><a class="<?php echo ($page == 'employees-list.php')?'active':'';?>" href="employees-list.php">Add Member</a></li>
                         <li><a class="<?php echo ($page == 'enrollment.php')?'active':'';?>" href="enrollment.php">Enrollment</a></li>
                         <li><a class="<?php echo ($page == 'renewal-and-cancellation.php')?'active':'';?>" href="renewal-and-cancellation.php">Renewal & Cancellations</a></li>
-                        <!-- <li><a class="<?php echo ($page == 'client-profile.php')?'active':'';?>" href="client-profile.php">Profile Overview</a></li> -->
+                        <li><a class="<?php echo ($page == 'client-profile.php')?'active':'';?>" href="client-profile.php">Profile Overview</a></li>
                         <li><a class="<?php echo ($page == 'attendance-tracking.php')?'active':'';?>" href="attendance-tracking.php">Attendance Tracking</a></li>
                         <li><a class="<?php echo ($page == 'personalized-recommendation.php')?'active':'';?>" href="personalized-recommendation.php">Personalized Recommendation</a></li>
                    </ul>
-                </li>
-                <!-- //* CUSTOMER MANAGEMENT -->
+                </li> -->
+                
+                 <hr style="border: 0; border-top: 1px solid #A0A0A0; margin: 10px 0;">
 
-                <li class="menu-title"> 
-                    <span>Transaction</span>
+                 <!-- //* TRANSACTION -->
+                <li class="submenu">
+                    <a href="#"><i class="la la-rocket"></i> <span>Transaction</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a class="<?php echo ($page == 'pos.php') ? 'active' : ''; ?>" href="pos.php">Point of Sale</a></li>
+                    </ul>
                 </li>
+                <hr style="border: 0; border-top: 1px solid #A0A0A0; margin: 10px 0;">
+                
 
-                <!-- //* POINT OF SALE -->
-                 <li class="<?php echo ($page == 'pos.php')?'active':'';?>" href="pos.php"> 
-                    <a href="pos.php"> <i class="la la-cash-register"></i> <span>Point of Sale</span></a>
+                <!-- //* UTILITIES -->
+                <li class="submenu">
+                    <a href="#"><i class="la la-tools"></i> <span>Utilities</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a class="<?php echo ($page == 'discount.php') ? 'active' : ''; ?>" href="discount.php">Discounts</a></li>
+                        <li><a class="<?php echo ($page == 'stock-in.php') ? 'active' : ''; ?>" href="stock-in.php">Stock In</a></li>
+                        <li><a class="<?php echo ($page == 'stock-out.php') ? 'active' : ''; ?>" href="stock-out.php">Stock Out</a></li>
+                        <li><a class="<?php echo ($page == 'archive.php') ? 'active' : ''; ?>" href="archive.php">Archive</a></li>
+                    </ul>
                 </li>
-                 <!-- //* POINT OF SALE -->
+                <hr style="border: 0; border-top: 1px solid #A0A0A0; margin: 10px 0;">
 
-                <li class="menu-title"> 
-                    <span>Utilities</span>
-                </li>
-                <li class="<?php echo ($page == 'discount.php')?'active':'';?>" href="discount.php"> 
-                    <a href="discount.php"> <i class="la la-gem"></i> <span>Discounts</span></a>
-                    <li><a class="<?php echo ($page == 'stock-in.php')?'active':'';?>" href="stock-in.php">Stock In</a></li>
-                    <li><a class="<?php echo ($page == 'stock-out.php')?'active':'';?>" href="stock-out.php">Stock Out</a></li>
-                </li>
-                <li class="<?php echo ($page == 'archive.php')?'active':'';?>" href="archive.php"> 
-                    <a href="archive.php"> <i class="fas fa-archive"></i> <span>Archive</span></a>
-                </li>
 
                 <!-- //* REPORTS -->
                  <li class="submenu">
@@ -84,7 +94,7 @@ $page = $components[2];
                         <li><a class="<?php echo ($page == 'attendance-reports.php')?'active':'';?>" href="attendance-reports.php">Attendance Reports</a></li>
                     </ul>
                 </li>
-                <!-- //* REPORTS -->
+                <hr style="border: 0; border-top: 1px solid #A0A0A0; margin: 10px 0;">
 
                  <!-- //* YOU CAN DELETE THIS -->
                  <li class="submenu">
