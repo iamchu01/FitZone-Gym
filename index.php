125c8e7df4ecfe1f6a36e167c5882775d537d904
Ronel Vincent Nuevo Chu <ronelvincentchu88@gmail.com>
2024-11-01 21:37:25 +0800

    chu

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
         <?php include 'layouts/title-meta.php'; ?>
 
         <?php include 'layouts/head-css.php'; ?>
-
     </head>
 
     <?php include 'layouts/body.php'; ?>
@@ -29,39 +28,32 @@
                             <p class="account-subtitle">ADMIN</p>
                             
                             <!-- Account Form -->
-                            <form action="admin-dashboard.php">
-                                <div class="form-group">
-                                    <label>Email Address</label>
-                                    <input class="form-control" type="text" value="">
-                                </div>
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col">
-                                            <label>Password</label>
+                            <form action="authenticate.php" method="post">
+                                    <div class="form-group">
+                                        <label>Email Address</label>
+                                        <input class="form-control" type="text" name="email" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="row">
+                                            <div class="col">
+                                                <label>Password</label>
+                                            </div>
+                                            <div class="col-auto">
+                                                <a class="text-muted" href="forgot-password.php">
+                                                    Forgot password?
+                                                </a>
+                                            </div>
                                         </div>
-                                        <div class="col-auto">
-                                            <a class="text-muted" href="forgot-password.php">
-                                                Forgot password?
-                                            </a>
+                                        <div class="position-relative">
+                                            <input class="form-control" type="password" name="password" id="password" required>
+                                            <span class="fa fa-eye-slash" id="toggle-password"></span>
                                         </div>
                                     </div>
-                                    <div class="position-relative">
-                                        <input class="form-control" type="password" value="" id="password">
-                                        <span class="fa fa-eye-slash" id="toggle-password"></span>
+                                    <div class="form-group text-center">
+                                        <button class="btn btn-primary account-btn" type="submit">Login</button>
                                     </div>
-                                </div>
-                                <div class="form-group text-center">
-                                    <button class="btn btn-primary account-btn" type="submit">Login</button>
-                                </div>
+                                </form>
 
-                                
-                                <!-- //! REMOVED REGISTER LINK -->
-                                <!-- <div class="account-footer">
-                                    <p>Don't have an account yet? <a href="register.php">Register</a></p>
-                                </div> -->
-                                <!-- //! REMOVED REGISTER LINK -->
-                                 
-                            </form>
                             <!-- /Account Form -->
                             
                         </div>
